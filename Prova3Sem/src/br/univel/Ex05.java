6dd1fe118a512fb07102c810a255d761556f5a71
rtietjen2 <rtietjen2@WS-LAB08_137.univel.univel.br>
2017-07-06 19:56:53 -0300

    Primeiro Commit

---
+++ Prova3Sem/src/br/univel/Ex05.java
@@ -0,0 +1,36 @@
+package br.univel;
+
+interface Show{
+	void show(Integer num);
+}
+
+class A implements Show{
+	
+	@Override
+	public void show(Integer num) {
+		String str = String.valueOf(num);
+		int result = Integer.parseInt(str.substring(5,6));
+		if(result > 10){
+			result = result % 10;
+		}
+		System.out.println(result);
+		
+	}
+}
+class B extends A{
+	public void show(Integer num) {
+		super.show(num + 1000);
+	};
+}
+
+public class Ex05 {
+	public static void main(String[] args) {
+		execute(167652);
+	}
+
+	private static void execute(Integer num) {
+		Show s = new B();
+		s.show(num);
+	}
+
+}
---
+++ Prova3Sem/src/br/univel/Ex09.java
@@ -0,0 +1,37 @@
+package br.univel;
+enum Valid{
+	A{
+		@Override
+		void calcular(int i) {
+			System.out.println(i / 2);
+			
+		}
+	},
+	B{
+		@Override
+	void calcular(int i) {
+		System.out.println(i / 3);
+		
+	}
+		
+	};
+		
+	abstract void calcular(int i);
+}
+
+public class Ex09 {
+	public static void main(String[] args) {
+		execute(167652);
+	
+	}
+	private static void execute(Integer ii) {
+		int idx = ii % 2;
+		Valid v;
+		if(idx == 0)
+			v = Valid.A;
+		else
+			v = Valid.B;
+		v.calcular(ii);
+	}
+
+}
